c49477c14f9b52d58ec0ff192473886758014938
Amer Nofal <a.nofal@bankaletihad.com>
2024-05-10 18:54:49 +0000

    end of dictionary

--- 03/03_04/03_04b/main.py
+++ 03/03_04/03_04b/main.py
@@ -1,3 +1,6 @@
+# Create a new dictionary with no None values.
+# This code has O(n) because we iterate through the dictionary only once.
+
 user_preferences = {
     "timezone": "GMT",
     "language": "English",
@@ -8,7 +11,12 @@ user_preferences = {
 
 
 def update_preferences(user_pref):
-    return {}
-
+    if user_pref == None:
+        return None
+    updated_user_pref = {}
+    for key, value in user_pref.items():
+        if user_pref[key] != None:
+            updated_user_pref[key] = value
+    return updated_user_pref
 
 print(update_preferences(user_preferences))
